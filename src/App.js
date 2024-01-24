4b8900242ab246029b1c6cfd4c10286e3aaf08a5
Gonzalo Orsi <gonzaloorsi@gmail.com>
2024-01-24 00:29:09 -0300

    CSS Module example Buttons - flexible

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Button from './components/Button/Button';
 
 function App() {
   return (
-    <>
+    <div>
     <Button>Hola mundo!</Button>
     <Button>Desde 1985</Button>
     <Button type="success">Interneteando</Button>
-    </>
+    </div>
+
   );
 }
 
